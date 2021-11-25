18ac741a01741dba618f57764348657ccbd37fe3
Sherlyson <sherlyson.silvagoncalves@gmail.com>
2021-11-25 17:33:09 -0300

    alteração no 65

--- ex065.py
+++ ex065.py
@@ -1,19 +1,17 @@
-i = 0
-somatoria = 0
+i = menor = maior = somatoria = 0
 opc = 'S'
-menor = 0
-maior = 0
 while opc not in 'Nn':
     n = int(input("Insira um número: "))
     i += 1
+    somatoria += n
     if i == 1:
         maior = n
         menor = n
-    if n < menor:
-        menor = n
-    if n > maior:
-        maior = n
-    somatoria += n
+    else:
+        if n < menor:
+            menor = n
+        if n > maior:
+            maior = n
     opc = str(input("Você deseja continuar? (S/N) ")).upper().strip()
 print("Você digitou {} números e a média foi {}".format(i, somatoria/i))
 print("O maior valor inserido foi {} e o menor foi {}".format(maior, menor))
